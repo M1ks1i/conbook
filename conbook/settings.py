6cf47e8edfd440b6c2c1406eaf29479dd956b425
M1ks1i <krilica19@gmail.com>
2025-11-20 17:16:45 +0100

    added navbar

--- conbook/settings.py
+++ conbook/settings.py
@@ -54,12 +54,12 @@ ROOT_URLCONF = 'conbook.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.templates.context_processors.request',
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
